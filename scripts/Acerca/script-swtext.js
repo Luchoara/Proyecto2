27e64cd55ef59c36be89d75518e6aa25d35af7ab
Pinto <nclpnt0528@gmail.com>
2024-02-09 12:39:34 -0300

    intro star wars terminada

--- scripts/Acerca/script-swtext.js
+++ scripts/Acerca/script-swtext.js
@@ -1,19 +1,22 @@
 document.getElementById("startButton").addEventListener("click", function() {
     document.getElementById("sw-text").style.animationPlayState = "running";
     document.getElementById("sw-audio").play(); // Reproducir el audio
-    document.getElementById("startButton").style.display = "none";
+    document.getElementById("startButton").style.animationPlayState = "running";
     document.getElementById("sw-controls-box").style.display = "block";
   });
 
   document.getElementById("pauseButton").addEventListener("click", function() {
     var swText = document.getElementById("sw-text");
+    var swTitle = document.getElementById("startButton");
     var swAudio = document.getElementById("sw-audio");
   
     if (swText.style.animationPlayState === "running") {
       swText.style.animationPlayState = "paused";
+      swTitle.style.animationPlayState = "paused";
       swAudio.pause(); // Pausar el audio
     } else {
       swText.style.animationPlayState = "running";
+      swTitle.style.animationPlayState = "running";
       swAudio.play(); // Reanudar el audio
     }
   });

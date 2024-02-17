5553443b2426831b630d625d1860c37488b0c1f7
Lucho <cheluiviajes@gmail.com>
2024-02-17 18:21:09 -0300

    edit reset_pass.js

--- scripts/usuario/reset_pass.js
+++ scripts/usuario/reset_pass.js
@@ -40,3 +40,32 @@ function isValidPassword(password) {
   console.log("contraseña validada")
   return regex.test(password);
 }
+
+
+/* Enviar email para recuperar Pass */
+
+if (userDataDec) {
+				// userData = JSON.parse(userData);
+
+				if (resetEmail === userDataDec.email || resetPassUsuario === userDataDec.nombreUsuario) {
+					
+					//* print para checkeo -- ELIMINAR --
+					alert("¡Envio exitoso!");
+				
+					// enviar un link para resetear el password usando smtpjs
+
+					//* Salta la alerta pero no llega nunca el email
+						Email.send({
+							SecureToken: "6ebc8191-139d-434c-ad01-cc5a3a93f1a7",
+							To : userDataDec.email,
+							From : "devStarWatch@gmail.com",
+							Subject : "Recuperar contraseña",
+							Body : 'body del email'
+						}).then(alert(`mensaje enviado a: ${userDataDec.email}`));
+
+				} else {
+					alert("Error, algo salio mal");
+				}
+			} else {
+				alert("El usuario no existe. Por favor, regístrate.");
+			}		

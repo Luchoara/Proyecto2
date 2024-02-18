535c400e6cabac658799842940ea573637099e06
Lucho <cheluiviajes@gmail.com>
2024-02-18 17:59:02 -0300

    email.js ok

--- scripts/usuario/email.js
+++ scripts/usuario/email.js
@@ -8,7 +8,7 @@ let emailFormulario = document.getElementById("emailFP").value;
 let usuarioDB = JSON.parse(localStorage.getItem(usuarioFormulario));
 // let emailDB = JSON.parse(localStorage.getItem(emailFormulario));
 
-let btn = document.getElementById('button');
+let btn = document.getElementById("button");
 
 //* listener para el formulario
 formularioReset.addEventListener("submit", function (e) {
@@ -18,24 +18,25 @@ formularioReset.addEventListener("submit", function (e) {
 	if (usuarioDB) {
 		//* Si los emails coinciden...
 		if (emailFormulario === usuarioDB.email) {
-			
-		//* Parametros para el envio del email
-		const serviceID = 'service_tvkcc19';
-		const templateID = 'template_zaxhkf9';
-		let parametrosEmail = {
-			to_name: usuarioFormulario,
-			to_email: emailFormulario,
-		}
-		
-		emailjs.send(serviceID, templateID, parametrosEmail)
-    .then(() => {
-      btn.value = 'Send Email';
-			//* CHECKPOINT
-      alert('Enviado, revisa tu casilla de correo!');
-    }, (err) => {
-      btn.value = 'Send Email';
-      alert(JSON.stringify(err));
-    });
+			//* Parametros para el envio del email
+			const serviceID = "service_tvkcc19";
+			const templateID = "template_zaxhkf9";
+			let parametrosEmail = {
+				to_name: usuarioFormulario,
+				to_email: emailFormulario,
+			};
+
+			emailjs.send(serviceID, templateID, parametrosEmail).then(
+				() => {
+					btn.value = "Send Email";
+					//* CHECKPOINT
+					alert("Enviado, revisa tu casilla de correo!");
+				},
+				(err) => {
+					btn.value = "Send Email";
+					alert(JSON.stringify(err));
+				}
+			);
 		} else {
 			alert("Error, algo salio mal");
 		}
